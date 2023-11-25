e96dd4c722e042dc7410531e6bdfd2924abff618
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2023-11-25 13:45:56 -0300

    add visual keymaps

--- lvim/config.lua
+++ lvim/config.lua
@@ -2,10 +2,12 @@ lvim.colorscheme = "rose-pine"
 lvim.transparent_window = true
 
 lvim.plugins = {
-  { 'rose-pine/neovim', name = 'rose-pine' }
+  { 'rose-pine/neovim', name = 'rose-pine' },
 }
 
 vim.api.nvim_create_autocmd('ColorScheme', {
   command = [[highlight CursorLine guibg=NONE guifg=NONE cterm=underline]]
 })
 
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
