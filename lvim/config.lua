667a8c2cace7e47f99b6a82ce4b728040ea518b1
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2023-11-25 13:51:29 -0300

    add float terminal lunar vim

--- lvim/config.lua
+++ lvim/config.lua
@@ -3,6 +3,7 @@ lvim.transparent_window = true
 
 lvim.plugins = {
   { 'rose-pine/neovim', name = 'rose-pine' },
+
 }
 
 vim.api.nvim_create_autocmd('ColorScheme', {
@@ -11,3 +12,8 @@ vim.api.nvim_create_autocmd('ColorScheme', {
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Terminal",
+  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" }
+}
