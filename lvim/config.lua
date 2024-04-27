a67e5215bdd17bf690d69214e5737b905f0b1369
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-04-27 13:46:31 -0300

    update

---
+++ lvim/config.lua
@@ -0,0 +1,21 @@
+lvim.transparent_window = true
+
+lvim.plugins = {
+
+  {'rose-pine/neovim', name = 'rose-pine'},
+  {'ThePrimeagen/vim-be-good'},
+  {'mattn/emmet-vim'},
+}
+
+lvim.colorscheme = 'rose-pine'
+vim.api.nvim_create_autocmd('ColorScheme', {
+  command = [[highlight CursorLine guibg=NONE guifg=NONE cterm=underline]]
+})
+
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Terminal",
+  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" }
+}
